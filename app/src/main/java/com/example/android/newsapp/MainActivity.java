1ecb888bb5503af41ce80f0a7b1c3bad41c4c336
Rodrigo Notario <rodrinot@ucm.es>
2020-06-27 02:01:15 +0200

    Update AsyncTask to Loader.

--- app/src/main/java/com/example/android/newsapp/MainActivity.java
+++ app/src/main/java/com/example/android/newsapp/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.android.newsapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.AsyncTaskLoader;
+import androidx.loader.content.Loader;
 
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -22,13 +26,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.MalformedURLException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
 
     /** URL for news data from Guardian APIs */
     private static final String GUARDIAN_URL =
@@ -53,6 +60,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportLoaderManager().initLoader(0, null, (LoaderManager.LoaderCallbacks<String>)this).forceLoad();
+
         newsList = new ArrayList<>();
         list_view = findViewById(R.id.list);
 
@@ -74,97 +83,169 @@ public class MainActivity extends AppCompatActivity {
             mEmptyStateTextView.setText(R.string.no_internet_connection);
         }
 
-        new Getnews().execute();
     }
 
-    private class Getnews extends AsyncTask<Void, Void, Void> {
+    @NonNull
+    @Override
+    public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
+        return new FetchData(this);
+    }
 
-        @Override
-        protected Void doInBackground(Void... arg0) {
-            HttpHandler sh = new HttpHandler();
-            String jsonString = "";
+    @Override
+    public void onLoadFinished(@NonNull Loader<String> loader, String jsonString) {
+
+        Log.e(TAG, "Response from url: " + jsonString);
+        if (jsonString != null) {
             try {
-                jsonString = sh.makeHttpRequest(createUrl(UriBuilder()));
-            } catch (IOException e) {
-                return null;
-            }
+                // Create a new JSONObject
+                JSONObject jsonObject = new JSONObject(jsonString);
 
-            Log.e(TAG, "Response from url: " + jsonString.toString());
-            if (jsonString != null) {
-                try {
-                    // DONE: Create a new JSONObject
-                    JSONObject jsonObject = new JSONObject(jsonString);
+                // Get the JSON Array node and headline it "results"
+                JSONObject response = jsonObject.getJSONObject("response");
 
-                    // DONE: Get the JSON Array node and headline it "results"
-                    JSONObject response = jsonObject.getJSONObject("response");
+                JSONArray results = response.getJSONArray("results");
 
-                    JSONArray results = response.getJSONArray("results");
+                if (results.length() > 0) {
+                    // looping through all Contacts
+                    for (int i = 0; i < results.length(); i++) {
+                        // DONE: get the JSONObject and its three attributes
+                        JSONObject c = results.getJSONObject(i);
+                        String headline = c.getString("webTitle");
+                        String section = c.getString("sectionName");
+                        String date = c.getString("webPublicationDate");
+                        String url = c.getString("webUrl");
 
-                    if (results.length() > 0) {
-                        // looping through all Contacts
-                        for (int i = 0; i < results.length(); i++) {
-                            // DONE: get the JSONObject and its three attributes
-                            JSONObject c = results.getJSONObject(i);
-                            String headline = c.getString("webTitle");
-                            String section = c.getString("sectionName");
-                            String date = c.getString("webPublicationDate");
-                            String url = c.getString("webUrl");
-
-                            JSONArray tags = c.getJSONArray("tags");
-                            JSONObject t = tags.getJSONObject(0);
-                            String author = "Anonymous";
-                            if (t.has("webTitle")) {
-                                author = t.getString("webTitle");
-                            }
-
-                            // tmp hash map for a single news
-                            HashMap<String, String> news = new HashMap<>();
-
-                            // add each child node to HashMap key => value
-                            news.put("headline", headline);
-                            news.put("section", section);
-                            news.put("author", author);
-                            news.put("date", date.substring(0, 10));
-                            news.put("url", url);
-
-                            // adding a news to our news list
-                            newsList.add(news);
+                        JSONArray tags = c.getJSONArray("tags");
+                        JSONObject t = tags.getJSONObject(0);
+                        String author = "Anonymous";
+                        if (t.has("webTitle")) {
+                            author = t.getString("webTitle");
                         }
-                    } else {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                mEmptyStateTextView.setText(R.string.no_news);
-                            }
-                        });
+
+                        // tmp hash map for a single news
+                        HashMap<String, String> news = new HashMap<>();
+
+                        // add each child node to HashMap key => value
+                        news.put("headline", headline);
+                        news.put("section", section);
+                        news.put("author", author);
+                        news.put("date", date.substring(0, 10));
+                        news.put("url", url);
+
+                        // adding a news to our news list
+                        newsList.add(news);
                     }
-                } catch (final JSONException e) {
-                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                } else {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            Toast.makeText(getApplicationContext(),
-                                    "Json parsing error: " + e.getMessage(),
-                                    Toast.LENGTH_LONG).show();
+                            mEmptyStateTextView.setText(R.string.no_news);
                         }
                     });
-
                 }
-
-            } else {
-                Log.e(TAG, "Couldn't get json from server.");
-                //mEmptyStateTextView.setText(R.string.no_news);
+            } catch (final JSONException e) {
+                Log.e(TAG, "Json parsing error: " + e.getMessage());
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         Toast.makeText(getApplicationContext(),
-                                "Couldn't get json from server.",
+                                "Json parsing error: " + e.getMessage(),
                                 Toast.LENGTH_LONG).show();
                     }
                 });
+
             }
 
-            return null;
+        } else {
+            Log.e(TAG, "Couldn't get json from server.");
+            mEmptyStateTextView.setText(R.string.no_news);
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(),
+                            "Couldn't get json from server.",
+                            Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+        // Hide the empty state text view as the loading indicator will be displayed
+        mEmptyStateTextView.setVisibility(View.GONE);
+
+        final ListAdapter adapter = new SimpleAdapter(MainActivity.this, newsList,
+                R.layout.list_item, new String[]{"headline", "section", "author", "date", "url"},
+                new int[]{R.id.headline, R.id.section, R.id.author, R.id.date, R.id.url});
+        list_view.setAdapter(adapter);
+
+        // Set a click listener to open the url when the list item is clicked on.
+        list_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // Open link
+                TextView textView = (TextView) view.findViewById(R.id.url);
+                String url = (String) textView.getText();
+
+                Uri uri = Uri.parse(url);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<String> loader) {
+
+    }
+
+    private static class FetchData extends AsyncTaskLoader<String> {
+
+        public FetchData(Context context) {
+            super(context);
+        }
+
+        @Override
+        public String loadInBackground() {
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String jsonStr = null;
+            String line;
+            try {
+                URL url = new URL(UriBuilder());
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) return null;
+
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                while ((line = reader.readLine()) != null) buffer.append(line);
+
+                if (buffer.length() == 0) return null;
+                jsonStr = buffer.toString();
+
+            } catch (IOException e) {
+                Log.e("MainActivity", "Error ", e);
+                return null;
+            } finally {
+                if (urlConnection != null) urlConnection.disconnect();
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("MainActivity", "Error closing stream", e);
+                    }
+                }
+            }
+
+            return jsonStr;
+        }
+
+        @Override
+        public void deliverResult(String data) {
+            super.deliverResult(data);
         }
 
         private String UriBuilder() {
@@ -178,43 +259,5 @@ public class MainActivity extends AppCompatActivity {
 
             return uriBuilder.toString();
         }
-
-        private URL createUrl(String stringUrl) {
-            URL url = null;
-            try {
-                url = new URL(stringUrl);
-            } catch (MalformedURLException exception) {
-                return null;
-            }
-            return url;
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-
-            // Hide the empty state text view as the loading indicator will be displayed
-            mEmptyStateTextView.setVisibility(View.GONE);
-
-            final ListAdapter adapter = new SimpleAdapter(MainActivity.this, newsList,
-                    R.layout.list_item, new String[]{"headline", "section", "author", "date", "url"},
-                    new int[]{R.id.headline, R.id.section, R.id.author, R.id.date, R.id.url});
-            list_view.setAdapter(adapter);
-
-            // Set a click listener to open the url when the list item is clicked on.
-            list_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
-                    // Open link
-                    TextView textView = (TextView) view.findViewById(R.id.url);
-                    String url = (String) textView.getText();
-
-                    Uri uri = Uri.parse(url);
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    startActivity(intent);
-                }
-            });
-        }
-
     }
 }
